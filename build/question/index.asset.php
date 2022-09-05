cd9ad3336bd748ce809f6d7f438696b67f995889
Evan Herman <evan.m.herman@gmail.com>
2022-09-05 09:38:46 -0400

    Fix stylelint errors

--- build/question/index.asset.php
+++ build/question/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bbdc962754b8927f7d08');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fb3daf76468fe9f50db4');
--- build/quiz/index.asset.php
+++ build/quiz/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ba4bea18b68fcc6c1621');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '86941eb9cab23df447fa');
